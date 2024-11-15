4b88e0b80ba8981f2b4d4f57af54765550c6409d
justtcoding9 <167090231+justtcoding9@users.noreply.github.com>
2024-11-15 13:12:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// variables: var let and cost
+
+// using var
+var x = 10; 
+console.log(x) //output 10
+
+// using let
+let y  = 20;
+console.log(y); // prints 20
+
+//using const
+const z = 30;
+console.log(z); // output 30
+
+// reassigning variables
+x = 15 // allowed
+y = 25; // Allowed
+// z = 35; //not Allowed, will throw an error
+
+console.log(x); // Output: 15
+console.log(y); // outut: 25
+console.log(z); // ouput: 30
